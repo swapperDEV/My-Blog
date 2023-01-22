5d3e8e25acc9e9b78d64c36539b68d262df82d3c
Wiktor Maciązek <75098062+swapperDEV@users.noreply.github.com>
2023-01-22 23:02:50 +0100

    navbar

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,11 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Button } from "../UI/Button/Button";
 import styles from "./navbar.module.scss";
 
 export const Navbar = () => {
+  const [prevScrollPos, setPrevScrollPos] = useState(0);
+  const [visible, setVisible] = useState(true);
+
+  const handleScroll = () => {
+    const currentScrollPos = window.scrollY;
+
+    if (currentScrollPos > prevScrollPos) {
+      setVisible(false);
+    } else {
+      setVisible(true);
+    }
+
+    setPrevScrollPos(currentScrollPos);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+
+    return () => window.removeEventListener("scroll", handleScroll);
+  });
   return (
     <>
-      <div className={styles.navbar}>
+      <div className={!visible ? styles.line : styles.none}></div>
+      <div className={`${styles.navbar} ${!visible && styles.diving}`}>
         <section>
           <div className={styles.author}>
             <a>Wiktor</a>
